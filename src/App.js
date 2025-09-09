943900b4cb5c49eb1ca9d9f1201c4d8ec35094a2
rohyeonwoo <gusfo2208@gmail.com>
2025-09-10 03:43:05 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileCard from './ProfileCard';
+
+function App(){
+    return(
+        <div style={{ padding:20 }}>
+            <ProfileCard name="노연우" studentId="2020103076" major="인공지능학과"/>
+        </div>
+    );
+}
+
+export default App;
